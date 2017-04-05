e0fe99a70e74311da56b09f7fcbc9434aee9dd3d
lanecatm <lanecatm@sjtu.edu.cn>
2017-04-05 19:42:46 +0800

    prepare to change sql

--- db/migrate/back_up/20170326050034_change_process_id_for_process_information.rb
+++ db/migrate/back_up/20170326050034_change_process_id_for_process_information.rb
@@ -1,7 +1,5 @@
 class ChangeProcessIdForProcessInformation < ActiveRecord::Migration
   def change
-
-
       add_reference :process_informations, :process, polymorphic: false, index: true
   end
 end
